d0e4c7261b9c6962cfc92f2ec907f65bd116f419
Hadrien Huvelle <hhu@baboum.be>
2024-01-30 12:01:37 +0100

    Working

--- cii/schema/D16B SCRDM (Subset)/coupled clm/CII/uncefact/data/standard/validate.sh
+++ cii/schema/D16B SCRDM (Subset)/coupled clm/CII/uncefact/data/standard/validate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ## You may need to install  python -m xsd_validator 
-python -m xsd_validator './cii/schema/D16B SCRDM (Subset)/coupled clm/CII/uncefact/data/standard/CrossIndustryInvoice_100pD16B.xsd'  './cii/schema/D16B SCRDM (Subset)/coupled clm/CII/uncefact/data/standard/InvoiceSample.xml'
+python -m xsd_validator './cii/schema/D16B SCRDM (Subset)/coupled clm/CII/uncefact/data/standard/OcmsCrossIndustryInvoice_100pD16B.xsd'  './cii/schema/D16B SCRDM (Subset)/coupled clm/CII/uncefact/data/standard/InvoiceSample.xml'
